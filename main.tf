ac516bc760c80deb19b18a28c1e58d6833b057e4
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-06-11 17:20:07 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,14 +49,13 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name               = "DevopsTRF"
 
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
+              echo "<p style="text-align:center"><b>FIAP - POS TECH!!!</b></p>" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
