6a77561db46c1ea5b154b14774fe030c13ecbf5f
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-06-11 17:00:14 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,9 +48,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  tags = {
-    name = "DevopsTRF"
-  }
+  name = "DevopsTRF"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
