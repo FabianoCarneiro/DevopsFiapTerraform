a522faf77df59ac9a07fa75d1eab2a54f952f4b4
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2024-09-23 21:21:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "<style> body {background-color: blue;}</style><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
+              echo "<style> body {background-color: black;}</style><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
@@ -79,7 +79,7 @@ resource "aws_security_group" "web-sg" {
 }
 
 resource "aws_iam_user" "new_user" {
-  name = "example_user" # Nome do novo usuário
+  name = "User Fiap" # Nome do novo usuário
 
   tags = {
     Name = "Example User"
