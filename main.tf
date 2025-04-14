249f627b79e34a637d2313bad0ae5cc9250ed077
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2025-04-14 20:54:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,11 +79,10 @@ resource "aws_security_group" "web-sg" {
 
 resource "aws_iam_user" "new_user" {
 
-  name = "Usuario_Impacta_Full08" # Nome do novo usuário
+  name = "Usuario_Impacta_Full09" # Nome do novo usuário
 
   tags = {
-    Name = "AS08"
-
+    Name = "FULL09"
   }
 }
 
